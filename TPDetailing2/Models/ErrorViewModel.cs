d6a4e5f154d7aa193e594696f743cc31af275cba
fazioSantiago <faziosantiago1@gmail.com>
2022-11-21 19:59:55 -0300

    Agregar archivos de proyecto.

---
+++ TPDetailing2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TPDetailing2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
